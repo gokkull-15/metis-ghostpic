fe6996cc169198ee3f6239e1a9107635d97d4352
gokkull-15 <gokkull04@gmail.com>
2025-08-22 00:19:24 +0530

    update1

--- src/const.js
+++ src/const.js
@@ -1,2 +1,2 @@
-export const BD_PORT = 'https://backend-9gd6b5ma3-gokkull04s-projects.vercel.app';
+export const BD_PORT = 'https://backend-96s2ko6r3-gokkull04s-projects.vercel.app';
 export const AI_URL_PORT = "https://ghost-test-qkc2.onrender.com";
